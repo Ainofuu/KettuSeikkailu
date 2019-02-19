d8ccfa3fb13e1f82341b15933aabe53b20fe4f75
LAPTOP-2N8VPFHF\Jer Jer Binks <jerri.ahonen@cs.tamk.fi>
2019-02-19 21:32:31 +0200

    added tree. started player.cs

---
+++ Assets/Code/Interactable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Interactable
+{
+    public string name;
+    public string popupInstructions;
+    // TODO: make something of this.
+}
---
+++ Assets/Code/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject interactionPopup;
+    public ParticleSystem campfire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        campfire.Stop();
+        interactionPopup.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Interactable"))
+        {
+            interactionPopup.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                campfire.Play();
+            }
+        }
+    }
+}
