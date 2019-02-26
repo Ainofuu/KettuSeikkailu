6340f4403e354d8695f02b7e15e9e46a525f7cb6
JerJerBinks <jerri.ahonen@gmail.com>
2019-02-26 15:56:50 +0200

    Enemy Attacks.

---
+++ Assets/Code/CharacterCombat.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterStats))]
+public class CharacterCombat : MonoBehaviour
+{
+    // Implement this in CharacterStats;
+    public float attackSpeed = 1f;
+    private float attackCooldown = 0f;
+
+    public float attackDelay = 0.6f;
+
+    public event System.Action OnAttack;
+
+    CharacterStats myStats;
+
+    private void Start()
+    {
+        myStats = GetComponent<CharacterStats>();
+    }
+
+    private void Update()
+    {
+        if (attackCooldown > 0)
+            attackCooldown -= Time.deltaTime;
+    }
+
+    public void Attack(CharacterStats targetStats)
+    {
+        if (attackCooldown <= 0f)
+        {
+            StartCoroutine(DoDamage(targetStats, attackDelay));
+
+            if (OnAttack != null)
+                OnAttack();
+
+            attackCooldown = 1f / attackSpeed;
+        }
+    }
+
+    IEnumerator DoDamage(CharacterStats targetStats, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        targetStats.TakeDamage(myStats.damage.GetValue());
+    }
+}
